ab21310951f4d0d2d598148adfcee4d77f64babc
quan.le <quan.le@fii-na.com>
2022-08-27 12:01:16 +0700

    Fix urls

--- wedding/urls.py
+++ wedding/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     # path(r'^$', views.Home.as_view(), name='home'),
-    path('home', views.Home.as_view(), name='home'),
+    # path('home', views.Home.as_view(), name='home'),
+    path('', views.Home.as_view(), name='home'),
     path('home/hanoi', views.hanoi, name='hanoi'),
     path('home/engagement', views.engagement, name='engagement')
 ]
