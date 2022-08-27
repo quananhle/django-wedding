541fccf8622a0fd69f4c3e5d4a83999bf629df71
quan.le <quan.le@fii-na.com>
2022-08-27 10:46:50 +0700

    Allow host

--- bigday/settings.py
+++ bigday/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u7!-y4k1c6b44q507nr_l+c^12o7ur++cpzyn!$65w^!gum@h%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['quanthu.pythonanywhere.com']
 
 
 # Application definition
