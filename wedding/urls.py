8c50561a1a971c0f2a2a50dc0d253fba20077302
quan.le <quan.le@fii-na.com>
2022-08-28 16:22:37 +0700

    Change Save the Dates to photo slider

--- wedding/urls.py
+++ wedding/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('home/hanoi', views.hanoi, name='hanoi'),
     path('home/engagement', views.engagement, name='engagement'),
-    path('home/wedding', views.wedding, name='wedding-party')
+    path('home/wedding', views.wedding, name='wedding-party'),
+    path('home/save-the-dates', views.save_the_date, name='save-the-dates-photos')
 ]
