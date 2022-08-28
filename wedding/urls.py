a04b0a5e89fade937f69dae24a50dd3d6796ac10
quan.le <quan.le@fii-na.com>
2022-08-28 13:38:24 +0700

    Wedding photos

--- wedding/urls.py
+++ wedding/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # path('home', views.Home.as_view(), name='home'),
     path('', views.Home.as_view(), name='home'),
     path('home/hanoi', views.hanoi, name='hanoi'),
-    path('home/engagement', views.engagement, name='engagement')
+    path('home/engagement', views.engagement, name='engagement'),
+    path('home/wedding', views.wedding, name='wedding-party')
 ]
